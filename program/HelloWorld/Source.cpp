ce556faa85c2c731909bcda9520dc04731eaa35c
jasensaint <jasensaint@gmail.com>
2016-02-08 21:51:12 -0700

    hello world

---
+++ program/HelloWorld/Source.cpp
@@ -0,0 +1,23 @@
+// File Prologue.......................................
+// Author: Michael Smith
+// Course: CS 2450
+// Project: Project #
+// Date: 02/08/16
+
+
+// I declare that the following source code was written
+// solely by me, or provided by the instructor.
+// I understand that copying any source
+// code, in whole or in part, constitutes cheating, and
+// that I will receive a zero in this assignment if I am
+// found in violation of this policy.
+
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "hello world" << std::endl;
+	system("pause");
+	return 1;
+}
